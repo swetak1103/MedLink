f06fcbfcc98fae727a1083a5967768faa90741be
swetak1103 <swetakn1109@gmail.com>
2024-04-18 11:11:58 +0530

    modified the hospital page and imported the json file

--- src/components/HospitalCard.js
+++ src/components/HospitalCard.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import hospitalsData from '../hospitals.json';
 
 
 const HospitalCard = ({ hospital }) => {
   return (
     <div className="hospital-card">
-      <h2>{hospital['name']}</h2>
-      <p><strong>address:</strong> {hospital.address}</p>
-      <p><strong>location:</strong> {hospital.location}</p>
-      <p><strong>beds:</strong> {hospital.contactInfo}</p>
-      <p><strong>contact_info:</strong> {hospital.beds}</p>
+      <h2>{hospital.name}</h2>
+      <p><strong>Address:</strong> {hospital.address}</p>
+      <p><strong>Location:</strong> {hospital.location}</p>
+      <p><strong>Beds:</strong> {hospital.beds}</p>
+      <p><strong>Contact Info:</strong> {hospital.contact_info}</p>
     </div>
   );
 };
 
-export default HospitalCard;
+const HospitalList = () => {
+  return (
+    <div>
+      {hospitalsData.map((hospital, index) => (
+        <HospitalCard key={index} hospital={hospital} />
+      ))}
+    </div>
+  );
+};
+
+export default HospitalList;
--- src/components/HospitalDetails.js
+++ src/components/HospitalDetails.js
@@ -2,10 +2,9 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import img from "../utils/img/h1.jpeg";
-// import 'bootstrap/dist/css/bootstrap.min.css';
+
 const HospitalDetails = (props) => {
-  const { name, address, contactInfo, beds } = props;
-  console.log(name);
+  const { name, address, location, beds, contact_info } = props;
 
   return (
     <Card style={{ width: "18rem", margin: "10px" }}>
@@ -13,24 +12,12 @@ const HospitalDetails = (props) => {
       <Card.Body>
         <Card.Title>{name}</Card.Title>
         <Card.Text>{address}</Card.Text>
-        <Card.Text>BedsAvailability: {beds}</Card.Text>
-        <Button variant="primary">{contactInfo}</Button>
+        <Card.Text>Beds Availability: {beds}</Card.Text>
+        <Button variant="primary">{contact_info}</Button>
+        <Card.Text>{location}</Card.Text>
       </Card.Body>
     </Card>
   );
-
-  // return (
-  //     <div style={{
-  //         background:"blue",
-  //         margin:"10px",
-  //     }}>
-  //         <h1>{name}</h1>
-  //         <h1>{address}</h1>
-  //         <h1>{location}</h1>
-  //         <h1>{beds}</h1>
-  //         <h1>{contactInfo}</h1>
-  //     </div>
-  // );
 };
 
 export default HospitalDetails;
